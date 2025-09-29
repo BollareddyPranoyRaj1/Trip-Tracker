e6602e1d3b751dcdce388ce610fce27f62eedf9f
BollareddyPranoyRaj1 <bollareddypranoyraj@gmail.com>
2025-09-29 19:10:46 +0530

    Initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const[trips,setTrips]=useState("");
+  const[input,setInput]=useState("");
+  const[isNudged,setIsNudged]=useState("");
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
